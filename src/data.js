fa5931eb090191734ba0e8c69b7560af97a07f00
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2021-03-02 14:56:51 -0800

    clear setup and set data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+import image1 from './images/lake-inniscarra-thumb.jpg';
+import imgA from './images/lake-inniscarra-trainer.jpg';
+import image2 from './images/performance-series-thumb.jpg';
+import imgB from './images/performance-series-trainer.jpg'
+import image3 from './images/slow-pulls-thumb.jpg';
+import imgC from './images/slow-pulls-trainer.jpg'
+import image4 from './images/20-minutes-to-toned-thumb.jpg';
+import imgD from './images/20-minutes-to-toned-trainer.jpg';
+import image5 from './images/charles-race-thumb.jpg';
+import imgE from './images/charles-race-trainer.jpg';
+import image6 from './images/full-body-hiit-thumb.jpg';
+import imgF from './images/full-body-hiit-trainer.jpg';
+import image7 from './images/kafue-river-thumb.jpg';
+import imgG from './images/kafue-river-trainer.jpg';
+import image8 from './images/shred-and-burn-thumb.jpg';
+import imgH from './images/slow-pulls-trainer.jpg';
+
+const data = [
+  { id:123, desc:'Lake-inniscarra, Ireland-Pyramid', largeImg: image1, smallImg: imgA, duration: '30:53', view: '6,248', workouts: 0 },
+  { id:456, desc: 'Performance-Series', largeImg: image2, smallImg: imgB, duration: '', view: '', workouts: 9 },
+  { id:789, desc: 'Slow Pulls and Fast Intervals', largeImg: image3, smallImg: imgC, duration: '44:13', view: '9,948', workouts: 0 },
+  { id:101112, desc: '20 Minutes to Toned', largeImg: image4, smallImg: imgD, duration: '', view: '', workouts: 12 },
+  { id:131415, desc: 'Charles Race, Boston, Massachusetts', largeImg: image5, smallImg: imgE, duration: '36:22', view: '8,648', workouts: 0 },
+  { id:161718, desc: 'Full Body HIIT Series', largeImg: image6, smallImg: imgF, duration: '', view: '', workouts: 12 },
+  { id:192021, desc: 'Kafur River, Zambia-Power Stroke Pyramid', largeImg: image7, smallImg: imgG, duration: '22:22', view: '4,660', workouts: 0 },
+  { id:222324, desc: 'Shred & Burn Series', largeImg: image8, smallImg: imgH, duration: '', view: '', workouts: 16 }
+];
+
+export default data;
